0e4f9944127604e148c3f945ac36186d7da57298
alhmet13 <alhmet13@gmail.com>
2026-07-21 11:51:23 +0300

    hotfix siyah yeter artık import ları unutuyon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { teamMembers } from "./data/teamData";
-import { PersonCard } from "./components/PersonCard";
+/* import { teamMembers } from "./data/teamData";
+import { PersonCard } from "./components/PersonCard"; */
 import "./App.css";
 
 function App() {
